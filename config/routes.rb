2445b9224eb8fd5d414ab865d14622945cdf011b
AkinaNagao <158458262+akina-nagao@users.noreply.github.com>
2024-05-19 13:39:02 +0000

    [update]admin publiccontroller model追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  
+  devise_for :admins, path: 'admin', controllers: {
+    sessions: 'admin/admins/sessions'
+  }
+  
+  devise_for :customers, controllers: {
+    sessions: 'public/customers/sessions',
+    registrations: 'public/customers/registrations'
+  }
+  
+  namespace :admin do
+    root 'home#top'
+  end
+  
+  scope module: :public do
+    root 'home#top'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
